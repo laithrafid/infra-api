93890a61255ada9b1f48a1a7e5d4fd393d2f938d
Laith <laith@bayt.cloud>
2022-03-30 23:43:18 -0400

    fixed typos

--- terraform/googlecloud/modules/gke_manage/providers.tf
+++ terraform/googlecloud/modules/gke_manage/providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">= 4.14.0"
     }
     google-beta = {
-      source = "hashicorp/googleworkspace"
+      source = "hashicorp/google-beta"
       version = ">= 4.14.0"
     }
   }
