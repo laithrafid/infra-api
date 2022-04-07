16f22ef60c3a2a1e64ba15a04c651dad040f9ecc
Laith <laith@bayt.cloud>
2022-04-07 01:35:30 -0400

    fix

--- terraform/googlecloud/modules/gke_create/providers.tf
+++ terraform/googlecloud/modules/gke_create/providers.tf
@@ -28,13 +28,13 @@ provider "google" {
   project               = var.project_name
   region                = var.region
   access_token          = var.GOOGLECLOUD_TOKEN
-  user_project_override = true
+  #user_project_override = true
   # credentials  = "${file(var.credentials_path)}"
 }
 provider "google-beta" {
   project               = var.project_name
   region                = var.region
   access_token          = var.GOOGLECLOUD_TOKEN
-  user_project_override = true
+  #user_project_override = true
   #  credentials  = "${file(var.credentials_path)}"
 }
