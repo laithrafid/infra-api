3c3dd4b1b5eda6be0c499d2f053a99a55a880540
Laith <laith@bayt.cloud>
2022-04-26 15:24:26 -0400

    moved terraform to seprate repo

--- terraform/aws/modules/create/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "id" {
-  value = aws_iam_access_key.kops.id
-  sensitive = true
-}
-output "secret" {
-  value = aws_iam_access_key.kops.secret
-  sensitive = true
-}
--- terraform/googlecloud/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cluster_name" {
-  value = module.gke_create.cluster_name
-}
-
-output "kubeconfig_path" {
-  value = var.write_kubeconfig ? abspath("${path.root}/kubeconfig") : "none"
-}
-
